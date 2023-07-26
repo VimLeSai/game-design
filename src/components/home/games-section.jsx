7bd080b239a9be807dac302d1e02e5e22d4757a6
rentbase-vimal-desai <desai.vimal250@gmail.com>
2023-07-26 14:59:03 +0530

    home screen load issue fixed

--- src/components/home/games-section.jsx
+++ src/components/home/games-section.jsx
@@ -6,7 +6,7 @@ const GamesSection = ({ title, games }) => {
       <b className="block mb-3">{title}</b>
       <div className="flex flex-row items-start justify-start gap-4 overflow-x-auto text-base">
         {games.map((game) => (
-          <div className="relative flex flex-col gap-2">
+          <div key={game.name} className="relative flex flex-col gap-2">
             <img className="rounded-xl w-[163px] h-[163px] object-cover" alt="" src={game.image} />
             <b className="">{game.name}</b>
           </div>
